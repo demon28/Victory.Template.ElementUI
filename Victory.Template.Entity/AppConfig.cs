3043e0f9882f4bec41be9a766dad5332864d86df
demon28 <274733956@qq.com>
2020-08-11 22:43:40 +0800

    添加项目文件。

---
+++ Victory.Template.Entity/AppConfig.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Victory.Core.Extensions;
+using Victory.Template.Entity.Model;
+
+
+namespace Victory.Template.Entity
+{
+    public class AppConfig
+    {
+
+        /// <summary>
+        /// 数据库连接字符串
+        /// </summary>
+        /// <returns></returns>
+        public static string ConnectionString() {
+            Victory.Core.Helpers.ConfigHelper configHelper = new Victory.Core.Helpers.ConfigHelper("appsettings.json");
+
+            var dbmodel = configHelper.Get<Model.DbModel>("ConnectionStrings");
+
+
+            string connString= dbmodel.DevelopDatabase;
+            if (dbmodel.DB== "ProductDatabase")
+            {
+                connString= dbmodel.ProductDatabase;
+            }
+
+            if (dbmodel.DB == "EnvironDatabase")
+            {
+                connString= dbmodel.EnvironDatabase;
+            }
+
+            if (dbmodel.DB == "DevelopDatabase")
+            {
+                connString= dbmodel.DevelopDatabase;
+            }
+
+
+            if (dbmodel.IsEncrypt)
+            {
+                connString= Victory.Core.Encrypt.Aes.DecryptString(connString, Core.Helpers.MachineHelper.GetCpuId());
+            }
+
+            return connString;
+        }
+
+
+        /// <summary>
+        /// Jwt
+        /// </summary>
+        public static JwtModel Jwt
+        {
+
+            get
+            {
+                Victory.Core.Helpers.ConfigHelper configHelper = new Victory.Core.Helpers.ConfigHelper("appsettings.json");
+
+                JwtModel model = configHelper.Get<Model.JwtModel>("Jwt");
+
+                return model;
+
+            }
+
+
+        }
+
+
+
+
+
+        /// <summary>
+        /// 是否忽略权限验证
+        /// </summary>
+        public static bool IgnoreAuthRight
+        {
+            get
+            {
+                Victory.Core.Helpers.ConfigHelper configHelper = new Victory.Core.Helpers.ConfigHelper("appsettings.json");
+
+                var key = configHelper.GetSingle("IgnoreAuthRight");
+
+                if (string.IsNullOrEmpty(key))
+                {
+                    return false;
+                }
+
+                return key.ToBool();
+            }
+
+        }
+
+        /// <summary>
+        /// 是否忽略Api签名验证
+        /// </summary>
+        public static bool IgnoreApiFilter
+        {
+            get
+            {
+                Victory.Core.Helpers.ConfigHelper configHelper = new Victory.Core.Helpers.ConfigHelper("appsettings.json");
+
+                var key = configHelper.GetSingle("IgnoreApiFilter");
+
+                if (string.IsNullOrEmpty(key))
+                {
+                    return false;
+                }
+
+                return key.ToBool();
+            }
+
+        }
+        
+        /// <summary>
+        /// 是否给初次登录的用户设置 默认的 用户角色
+        /// </summary>
+       public static bool IsSetDefautlRole
+        {
+            get
+            {
+                Victory.Core.Helpers.ConfigHelper configHelper = new Victory.Core.Helpers.ConfigHelper("appsettings.json");
+
+                var key = configHelper.GetSingle("IsSetDefautlRole");
+
+                if (string.IsNullOrEmpty(key))
+                {
+                    return false;
+                }
+
+                return key.ToBool();
+            }
+
+        }
+        /// <summary>
+        /// 是否开启全局异常捕捉过滤器
+        /// </summary>
+        public static bool IsExceptionFilter
+        {
+            get
+            {
+                Victory.Core.Helpers.ConfigHelper configHelper = new Victory.Core.Helpers.ConfigHelper("appsettings.json");
+
+                var key = configHelper.GetSingle("IsExceptionFilter");
+
+                if (string.IsNullOrEmpty(key))
+                {
+                    return false;
+                }
+
+                return key.ToBool();
+            }
+
+        }
+        
+
+    }
+}
---
+++ Victory.Template.Facade/LoginFacade.cs
@@ -0,0 +1,38 @@
+﻿using Victory.Template.DataAccess.CodeGenerator;
+using Victory.Template.Entity.CodeGenerator;
+
+namespace Victory.Template.Facade
+{
+    public class LoginFacade: FacadeBase
+    {
+            
+        public bool Login(string workId,string pwd,ref Tsys_User user)
+        {
+
+            Tsys_User_Da da = new Tsys_User_Da();
+
+            if (da.Where(s => s.Workid == workId).ToOne() == null)
+            {
+                this.Message = "用户工号不存在";
+                
+                return false;
+            }
+
+            user = da.Where(s => s.Workid == workId && s.Pwd == pwd).ToOne();
+
+            if (user==null)
+            {
+                this.Message = "密码错误";
+
+                return false;
+            }
+
+            return true;
+
+        }
+
+
+
+
+    }
+}
